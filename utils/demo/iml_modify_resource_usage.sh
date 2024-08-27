c1b15bac54ca493a305fd713c22f8ac0d2b7cc2a
adamzr2000 <adamzr2000@gmail.com>
2024-08-27 16:03:56 +0200

    update.

--- utils/demo/iml_modify_resource_usage.sh
+++ utils/demo/iml_modify_resource_usage.sh
@@ -9,3 +9,5 @@ curl -F file=@demo_pol_3_perf_isolation_different_ns-slice_1-robot.yml http://lo
 echo "Applying reduced resource usage policy to the background flow in network slice 1..."
 curl -F file=@demo_pol_3_perf_isolation_different_ns-slice_1-bg.yml http://localhost:5000/iml/update-policy
 
+# echo "Applying reduced resource usage policy to the simulated flows in network slice 2..."
+# curl -F file=@demo_pol_3_perf_isolation_different_ns-slice_2.yml http://localhost:5000/iml/update-policy
